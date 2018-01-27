2f3b532ea6ce2eaf3a2314879ed1fa3f4143a838
Samantha Klemm <samantha-klemm@uiowa.edu>
2018-01-27 18:39:34 -0500

    scaffold form view
    html, css, js & materialize

---
+++ index.js
@@ -0,0 +1,11 @@
+ 
+
+ // ...now that document has loaded, do these things
+ $(document).ready(function() {
+
+ 	// allow form field labels to animate
+    Materialize.updateTextFields();
+
+
+  });
+       
