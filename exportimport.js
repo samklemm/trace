320a37da7d91a555fcc3f3b874b290cd45e7b158
Margarita Gubanova <gubanova.marg@yandex.com>
2018-01-28 02:18:45 -0500

    JSON works for Person

--- exportimport.js
+++ exportimport.js
@@ -1,39 +1,4 @@
-  
-
-  function exportFromLocation() {
-    let streetnumber = document.getElementById('street_number');
-    let streetname = document.getElementById('street_name');
-    let city = document.getElementById('city');
-    let state = document.getElementById('state');
-    let zip = document.getElementById('zip');
-    let date = document.getElementById("date").value;
-
-  var selchbox = [];
-
-  var inpfields = document.getElementsByTagName("*");
-
-  for(var i=0; i< inpfields.length; i++) {
-    if(inpfields[i].type == 'checkbox' && inpfields[i].checked == true) 
-    	selchbox.push(inpfields[i].value);
-  }
-
-    let json1 = {
-      "street_num": streetnumber,
-      "street_name": streetname,
-      "city": city,
-      "state": state,
-      "zip": zip, 
-      "date": date,
-      "harrassment": selchbox
-    };
-    
-    alert("Here is a json location" + JSON.stringify(json1,1));  
-  }
-
-
-
-
-    function exportFromPerson() {
+function exportFromPerson() {
     let firstname =  document.getElementById("first_name").value;
     let lastname =  document.getElementById("last_name").value;
     let physical = document.getElementById("physical").value;
@@ -49,6 +14,7 @@
       selchbox.push(inpfields[i].value);
   }
 
+>>>>>>> 0ea7d6a8b8b40ba8519c07bf259553a91eb0161a
     let json2 = {
       "first_name": firstname,
       "last_name": lastname,
